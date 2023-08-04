76bcd65d42e60b7d150e64266ac8b9a974c3d193
Your Name <devshah152004@gmail.com>
2023-08-04 13:35:20 +0530

    change

--- src/Loader.js
+++ src/Loader.js
@@ -4,15 +4,15 @@ import "./Loader.css";
 export default function Loader() {
   return (
     <div style={{position:"absolute", top:"40%", minWidth:"100%"}}>
-      <div class="hourglassBackground">
-        <div class="hourglassContainer">
-          <div class="hourglassCurves"></div>
-          <div class="hourglassCapTop"></div>
-          <div class="hourglassGlassTop"></div>
-          <div class="hourglassSand"></div>
-          <div class="hourglassSandStream"></div>
-          <div class="hourglassCapBottom"></div>
-          <div class="hourglassGlass"></div>
+      <div className="hourglassBackground">
+        <div className="hourglassContainer">
+          <div className="hourglassCurves"></div>
+          <div className="hourglassCapTop"></div>
+          <div className="hourglassGlassTop"></div>
+          <div className="hourglassSand"></div>
+          <div className="hourglassSandStream"></div>
+          <div className="hourglassCapBottom"></div>
+          <div className="hourglassGlass"></div>
         </div>
       </div>
     </div>
